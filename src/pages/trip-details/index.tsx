48299cc440166b47579ab3aa29f5682965a75849
taynara veloso <taynarasantos2.3@hotmail.com>
2024-07-20 22:07:25 -0300

    :star: function open and close modal

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,6 @@
 import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
 
 interface TripDetailsPageProps{
   handleCloseGuestsInput: () => void
@@ -7,6 +9,16 @@ interface TripDetailsPageProps{
 export function TripDetailsPage({
   handleCloseGuestsInput,
 }: TripDetailsPageProps) {
+  const [isCreateActivityModal, setIsCreateActivityModal] = useState(false)
+
+  function handleOpenCreateActivityModal() {
+    setIsCreateActivityModal(true)
+  }
+
+  function handleCloseCreateActivityModal() {
+    setIsCreateActivityModal(false)
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -34,7 +46,7 @@ export function TripDetailsPage({
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button onClick={handleOpenCreateActivityModal} type="button" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               <Plus className="size-5"/>
               Cadastrar atividade
             </button>
@@ -132,6 +144,12 @@ export function TripDetailsPage({
           </div>
         </div>
       </main>
+
+      {isCreateActivityModal && (
+        <CreateActivityModal
+          handleCloseCreateActivityModal={handleCloseCreateActivityModal}
+        />
+      )}
     </div>
   )
 }
