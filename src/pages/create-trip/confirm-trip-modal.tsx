3dbb8be0c579d2b81f392cecca2062012d100a1f
taynara veloso <taynarasantos2.3@hotmail.com>
2024-07-24 16:56:46 -0300

    :star: button component - Modal - Confirm Trip

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { Mail, User, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
   handleCloseConfirmTripModal: () => void,
@@ -42,11 +43,9 @@ export function ConfirmTripModal({
               placeholder="Seu e-mail pessoal" 
               className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/> 
           </div>
-          <button
-            type="submit" 
-            className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
+          <Button size="full">
             Confirmar criação da viagem
-          </button>
+          </Button>
         </form>
       </div>
     </div>
